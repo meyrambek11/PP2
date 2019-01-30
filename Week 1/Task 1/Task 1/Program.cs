2fcd281dc36b8c87d6154c3971621961a7ee0439
meyrambek11 <m_kadyrbekov@bk.ru>
2019-01-30 16:18:19 +0600

    just commit

---
+++ Week 1/Task 1/Task 1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string a = Console.ReadLine(); //As we can't directly insert integers, firstly, we need to read it as string;
+            int cnt = 0; // for counting amount of prime numbers
+            int t;  
+            int s = int.Parse(a); //Function "Parse" allows us to convert string to int;   
+            string[] A = Console.ReadLine().Split(' '); //create new array and read our s integers like string;
+            List<int> myArr = new List<int>(); //Create a new list;
+            List<int> arr = new List<int>(); //Create a new list;
+            for(int i = 0; i < s; i++)
+            {
+                t = int.Parse(A[i]); // We convert our string into int;
+                myArr.Add(t);  // adding every element of numb into new list
+            }
+            for (int j = 0; j < myArr.Count; j++) //to identify prime numbers
+            {
+                bool m = true; //I created it to determine the number of prime or not, initially all numbers are prime
+                for (int k = 2; k < myArr[j]; k++)
+                {
+                    if (myArr[j] % k == 0) // So that the number was prime, it should be divided by itself and one
+                    {
+                        m = false; 
+                        break;
+                    }
+                }
+                if (m == true && myArr[j] > 1)
+                {
+                    arr.Add(myArr[j]); // i add prime numbers to new list;
+                    cnt++;  // if number is prime then cnt++;
+                }
+            }
+            Console.WriteLine(cnt); // To show sum of primes;
+            for(int l = 0; l < arr.Count; l++)
+            {
+                Console.Write(arr[l] + " "); //to Show prime numbers;
+            }
+        }
+    }
+}
---
+++ Week 1/Task 2/Task 2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Student //We creat new class, then we add parametrs;
+    {
+        public string name;//We creat string for name of student; //"Public" allows гы to use parameters everywhere;
+        public string id; //We creat string for ID of student;
+        public string year; //We nead to read as string;
+        public Student(string name, string id) //create new function for constructors; 
+        {
+            this.name = name; // we use "this" to indecate our string
+            this.id = id;
+        }
+        public Student()
+        {
+            name = Console.ReadLine(); //read our strings;
+            id = Console.ReadLine();
+            year = Console.ReadLine();
+        }
+        public void Print() // "Void" will not return anything
+        {
+            int a = int.Parse(year); //We replace our string to int using "Parse";
+            int c = a + 1;
+            Console.WriteLine("Name: " + name); //to show our results
+            Console.WriteLine("Student's ID: " + id);
+            Console.WriteLine("Year of study: " + c);
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student Brother = new Student(); // to announce our class, to create new class;
+            Brother.Print(); // to induce function "Print" for new class "Brother";
+        }
+    }
+}
